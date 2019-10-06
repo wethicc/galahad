347ab6341951c30b8f3edaec8e841aafa9c8642e
Matt <mathew.beldon@hotmail.co.uk>
2019-10-06 20:33:10 +0100

    Matt - Adds Event and Group Models

---
+++ galahad/galahad/Models/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace galahad.Models
+{
+    class Event
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double lonX { get; set; }
+        public double lonY { get; set; }
+        public DateTime Date { get; set; }
+        public bool Repeated { get; set; }
+        public bool Warning { get; set; }
+        public DateTime WarningTime { get; set; }
+    }
+}
---
+++ galahad/galahad/Models/Group.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace galahad.Models
+{
+    class Group
+    {
+        public string Id { get; set; }
+        public List<Event> Events { get; set; }
+    }
+}
